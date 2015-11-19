8c59c0eece53b7148720e1121b66c6e3e419ce3c
smagic39 <smagic39@gmail.com>
2015-11-19 17:58:26 +0700

    add css,js,image, font for new design

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,151 +1,281 @@
-/***
- *
- * KLOUDS ORG
- *
- */
 
-(function (module) {
-    module.loader = function () {
-        jQuery(".status").fadeOut();
-        // will fade out the whole DIV that covers the website.
-        jQuery(".preloader").delay(1000).fadeOut("slow");
-    };
-    module.initVideo = function () {
-        $(".video-container").fitVids();
-    };
-    module.textEffect = function () {
-        $(".texts > li").hide();
-        setInterval(function () {
-            $('.texts > li:first')
-                .fadeOut(1000)
-                .next()
-                .fadeIn(1000)
-                .end()
-                .appendTo('.texts');
-        }, 3000);
+$(window).load(function() {
 
-    };
-    module.navPage = function () {
-        $('.main-navigation').onePageNav({
-            scrollThreshold: 0.2, // Adjust if Navigation highlights too early or too late
-            filter: ':not(.external)',
-            changeHash: true
+    "use strict";
+
+
+    
+//------------------------------------------------------------------------
+//                      COUNTER SCRIPT
+//------------------------------------------------------------------------
+   $('.timer').counterUp({
+            delay: 20,
+            time: 2500
         });
-    };
-    module.mainMediumNav = function () {
-        if ($("#home").length > 0) {
-            var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-            if (top > 40) $('.sticky-navigation').stop().css({"top": '0',"position": 'fixed',"width":"100%"});
-            else $('.sticky-navigation').stop().css({"position": 'relative'});
+
+
+
+//------------------------------------------------------------------------
+//                      NAVBAR SLIDE SCRIPT
+//------------------------------------------------------------------------      
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $("nav").height()) {
+            $("nav.navbar-slide").addClass("show-menu");
+        } else {
+            $("nav.navbar-slide").removeClass("show-menu");
+            $(".navbar-slide .navMenuCollapse").collapse({toggle: false});
+            $(".navbar-slide .navMenuCollapse").collapse("hide");
+            $(".navbar-slide .navbar-toggle").addClass("collapsed");
         }
-
-    };
-    module.mainMinNav = function () {
-        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-        if (top > 40) $('.sticky-navigation').stop().animate({"top": '0',"position": 'fixed'});
-
-        else $('.sticky-navigation').stop().css({"top": '-120',"position": 'fixed'});
-    };
-    module.mainSmallNav = function () {
-        $('.main-navigation a').on('click', function () {
-            $(".navbar-toggle").click();
-        });
-    };
-    module.fullScreen = function () {
-        $(".full-screen").css('min-height', $(window).height());
-    };
-    module.scrollTo = function () {
-        var scrollAnimationTime = 1200,
-            scrollAnimation = 'easeInOutExpo';
-        $('a.scrollto').bind('click.smoothscroll', function (event) {
-            event.preventDefault();
-            var target = this.hash;
-            $('html, body').stop().animate({
-                'scrollTop': $(target).offset().top
-            }, scrollAnimationTime, scrollAnimation, function () {
-                window.location.hash = target;
-            });
-        });
-    }
-    module.init = function () {
-        module.textEffect();
-        module.navPage();
-        module.initVideo();
-
-        jQuery("#responsive_headline").fitText();
-
-    };
-})(Klouds = {});
-
-/* =================================
- LOAD JS
- =================================== */
-
-jQuery(window).load(function () {
-    Klouds.loader();
+    });
+    
+//------------------------------------------------------------------------
+//                      NAVBAR HIDE ON CLICK (COLLAPSED) SCRIPT
+//------------------------------------------------------------------------      
+    $('.nav a').on('click', function(){ 
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").click()
+        }
+    });
+    
 })
 
-jQuery(function () {
-    //Klouds.mainMediumNav();
-    Klouds.init();
-    Klouds.fullScreen();
-    Klouds.scrollTo();
-
-    $(window).bind('resize', Klouds.fullScreen());
-    wow = new WOW(
-        {
-            mobile: false
-        });
-    wow.init();
-    $('#screenshots a').nivoLightbox({
-        effect: 'fadeScale',
-    });
-    $("#feedbacks").owlCarousel({
-
-        navigation: false, // Show next and prev buttons
-        slideSpeed: 400,
-        paginationSpeed: 800,
-        autoPlay: 5000,
-        singleItem: true
-    });
-    $('.expand-form').simpleexpand({
-        'defaultTarget': '.expanded-contact-form'
-    });
 
 
-    $(window).stellar({
-        horizontalScrolling: false
+
+$(document).ready(function(){
+            
+    "use strict";
+
+    
+    
+//------------------------------------------------------------------------
+//                      ANCHOR SMOOTHSCROLL SETTINGS
+//------------------------------------------------------------------------
+    $('a.smooth, .navbar .nav a').smoothScroll({speed: 1200});
+
+
+
+
+//------------------------------------------------------------------------  
+//                    MAGNIFIC POPUP(LIGHTBOX) SETTINGS
+//------------------------------------------------------------------------  
+              
+    $('.portfolio-list li').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
     });
+
+    
+
+    
+//------------------------------------------------------------------------
+//                      VIDEO BACKGROUND SETTINGS
+//------------------------------------------------------------------------
+if($('.video-bg')[0]) {
+    $(function() {
+        var BV = new $.BigVideo({container: $('.video-bg'), useFlashForFirefox:false});
+        BV.init();
+        if(navigator.userAgent.match(/iPhone|iPad|iPod|Android|BlackBerry|IEMobile/i)) {
+                BV.show('images/video_gag.jpg');
+        } else{
+            if (!!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0) {
+                BV.show('video/video_bg.ogv', {doLoop:true, ambient:true});
+            } else{
+                BV.show('video/video_bg.mp4', {doLoop:true, ambient:true, altSource:'video/video_bg.ogv'});
+            }
+            BV.getPlayer().on('loadedmetadata', function(){
+                $('#big-video-wrap video').fadeIn('slow');
+            });
+        }
+    }); 
+}
+
+
+        
+    
+//------------------------------------------------------------------------
+//                  SUBSCRIBE FORM VALIDATION'S SETTINGS
+//------------------------------------------------------------------------          
+    $('#subscribe_form').validate({
+        onfocusout: false,
+        onkeyup: false,
+        rules: {
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        errorPlacement: function(error, element) {
+            error.appendTo( element.closest("form"));
+        },
+        messages: {
+            email: {
+                required: "We need your email address to contact you",
+                email: "Please, enter a valid email"
+            }
+        },
+                    
+        highlight: function(element) {
+            $(element)
+        },                    
+                    
+        success: function(element) {
+            element
+            .text('').addClass('valid')
+        }
+    }); 
+    
+
+        
+                
+//------------------------------------------------------------------------------------
+//                      SUBSCRIBE FORM MAILCHIMP INTEGRATIONS SCRIPT
+//------------------------------------------------------------------------------------      
+    $('#subscribe_form').submit(function() {
+        $('.error').hide();
+        $('.error').fadeIn();
+        // submit the form
+        if($(this).valid()){
+            $('#subscribe_submit').button('loading'); 
+            var action = $(this).attr('action');
+            $.ajax({
+                url: action,
+                type: 'POST',
+                data: {
+                    newsletter_email: $('#subscribe_email').val()
+                },
+                success: function(data) {
+                    $('#subscribe_submit').button('reset');
+                    
+                    //Use modal popups to display messages
+                    $('#modalMessage .modal-title').html('<i class="icon icon-envelope-open"></i>' + data);
+                    $('#modalMessage').modal('show');
+                    
+                },
+                error: function() {
+                    $('#subscribe_submit').button('reset');
+                    
+                    //Use modal popups to display messages
+                    $('#modalMessage .modal-title').html('<i class="icon icon-ban"></i>Oops!<br>Something went wrong!');
+                    $('#modalMessage').modal('show');
+                    
+                }
+            });
+        }
+        return false; 
+    });
+      
+      
+      
+      
+//------------------------------------------------------------------------------------
+//                      CONTACT FORM VALIDATION'S SETTINGS
+//------------------------------------------------------------------------------------        
+    $('#contact_form').validate({
+        onfocusout: false,
+        onkeyup: false,
+        rules: {
+            name: "required",
+            message: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        errorPlacement: function(error, element) {
+            error.insertAfter(element);
+        },
+        messages: {
+            name: "What's your name?",
+            message: "Type your message",
+            email: {
+                required: "What's your email?",
+                email: "Please, enter a valid email"
+            }
+        },
+                    
+        highlight: function(element) {
+            $(element)
+            .text('').addClass('error')
+        },                    
+                    
+        success: function(element) {
+            element
+            .text('').addClass('valid')
+        }
+    });   
+
+
+
+
+//------------------------------------------------------------------------------------
+//                              CONTACT FORM SCRIPT
+//------------------------------------------------------------------------------------  
+    
+    $('#contact_form').submit(function() {
+        // submit the form
+        if($(this).valid()){
+            $('#contact_submit').button('loading'); 
+            var action = $(this).attr('action');
+            $.ajax({
+                url: action,
+                type: 'POST',
+                data: {
+                    contactname: $('#contact_name').val(),
+                    contactemail: $('#contact_email').val(),
+                    contactmessage: $('#contact_message').val()
+                },
+                success: function() {
+                    $('#contact_submit').button('reset');
+                    $('#modalContact').modal('hide');
+                    
+                    //Use modal popups to display messages
+                    $('#modalMessage .modal-title').html('<i class="icon icon-envelope-open"></i>Well done!<br>Your message has been successfully sent!');
+                    $('#modalMessage').modal('show');
+                },
+                error: function() {
+                    $('#contact_submit').button('reset');
+                    $('#modalContact').modal('hide');
+                    
+                    //Use modal popups to display messages
+                    $('#modalMessage .modal-title').html('<i class="icon icon-ban"></i>Oops!<br>Something went wrong!');
+                    $('#modalMessage').modal('show');
+                }
+            });
+        } else {
+            $('#contact_submit').button('reset')
+        }
+        return false; 
+    });           
 
 });
+//------------------------------------------------------------------------------------
+//                              Scroll to Top Button
+//------------------------------------------------------------------------------------  
 
-$(window).scroll(function () {
-    Klouds.mainMediumNav();
-})
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 100){
+            $('#to-top').stop().animate({
+                bottom: '30px'
+                }, 750);
+        }
+        else{
+            $('#to-top').stop().animate({
+               bottom: '-100px'
+            }, 750);
+        }
+    });
 
-
-/* Bootstrap Internet Explorer 10 in Windows 8 and Windows Phone 8 FIX */
-
-if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-    var msViewportStyle = document.createElement('style')
-    msViewportStyle.appendChild(
-        document.createTextNode(
-            '@-ms-viewport{width:auto!important}'
-        )
-    )
-    document.querySelector('head').appendChild(msViewportStyle)
-}
-/* COLLAPSE NAVIGATION ON MOBILE AFTER CLICKING ON LINK*/
-
-if (matchMedia('(max-width: 480px)').matches) {
-    Klouds.mainSmallNav();
-}
-
-if (matchMedia('(min-width: 992px), (max-width: 767px)').matches) {
-    Klouds.mainMediumNav();
-}
-
-if (matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
-    Klouds.mainMinNav();
-}
-
+    $('#to-top').click(function() {
+        $('html, body').stop().animate({
+           scrollTop: 0
+        }, 750, function() {
+           $('#to-top').stop().animate({
+               bottom: '-100px'
+           }, 750);
+        });
+    });
